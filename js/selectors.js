195b8e2f89a2310ed9514fe7698d96ce4938dbee
Kevin Minutti <minutti70@gmail.com>
2020-03-28 03:36:21 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,21 +1,21 @@
 // declare your functions here...
 
 function paragraphSelector() {
- $('p')
+ return $('p')
 }
 
 function lastImageSelector() {
- $('img:last')
+ return $('img:last')
 }
 
 function ninjaBabySelector() {
- $('#baby-ninja')
+ return $('#baby-ninja')
 }
 
 function divSelector() {
-$('.pics')
+return $('.pics')
 }
 
 function firstListItem() {
-$('ul li:first-child')
+return $('ul li:first-child')
 }
